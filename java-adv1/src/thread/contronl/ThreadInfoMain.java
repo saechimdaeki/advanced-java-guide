d4944395390f1446c6e786e598a9bd505f69dd7c
saechimdaeki/junseong <anima94@kakao.com>
2024-09-02 22:14:38 +0900

    study: 스레드 기본 정보

---
+++ java-adv1/src/thread/contronl/ThreadInfoMain.java
@@ -0,0 +1,27 @@
+package thread.contronl;
+
+import static util.MyLogger.log;
+
+import thread.start.HelloRunnable;
+
+public class ThreadInfoMain {
+
+    public static void main(String[] args) {
+        // main 스레드
+        Thread mainThread = Thread.currentThread();
+        log("mainThread = " + mainThread);
+        log("mainThread.threadId()= " + mainThread.threadId());
+        log("mainThread.getName()= " + mainThread.getName());
+        log("mainThread.getPriority()= " + mainThread.getPriority());
+        log("mainThread.getThreadGroup()= " + mainThread.getThreadGroup());
+        log("mainThread.getState()= " + mainThread.getState());
+
+        Thread myThread = new Thread(new HelloRunnable(), "myThread");
+        log("myThread = " + myThread);
+        log("myThread.threadId()= " + myThread.threadId());
+        log("myThread.getName()= " + myThread.getName());
+        log("myThread.getPriority()= " + myThread.getPriority());
+        log("myThread.getThreadGroup()= " + myThread.getThreadGroup());
+        log("myThread.getState()= " + myThread.getState());
+    }
+}
