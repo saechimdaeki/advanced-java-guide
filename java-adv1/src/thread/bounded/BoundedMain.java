db26a4bcd7c597f743027663019afdfd8921fd67
saechimdaeki/junseong <anima94@kakao.com>
2024-09-22 14:06:11 +0900

    study: blockingQueue 추가적 기능

--- java-adv1/src/thread/bounded/BoundedMain.java
+++ java-adv1/src/thread/bounded/BoundedMain.java
@@ -17,7 +17,9 @@ public class BoundedMain {
 
 //        final BoundedQueue queue = new BoundedQueueV6_1(2);
 //        final BoundedQueue queue = new BoundedQueueV6_2(2);
-        final BoundedQueue queue = new BoundedQueueV6_3(2);
+//        final BoundedQueue queue = new BoundedQueueV6_3(2);
+        final BoundedQueue queue = new BoundedQueueV6_4(2);
+
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
         producerFirst(queue); // 생산자 먼저 실행
