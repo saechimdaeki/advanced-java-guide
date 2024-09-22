078844f7d4dc806f050caf322c0d2573a907d95d
saechimdaeki/junseong <anima94@kakao.com>
2024-09-22 13:59:07 +0900

    study: blockingQueue

--- java-adv1/src/thread/bounded/BoundedMain.java
+++ java-adv1/src/thread/bounded/BoundedMain.java
@@ -13,8 +13,11 @@ public class BoundedMain {
 //        final BoundedQueue queue = new BoundedQueueV2(2);
 //        final BoundedQueue queue = new BoundedQueueV3(2);
 //        final BoundedQueue queue = new BoundedQueueV4(2);
-        final BoundedQueue queue = new BoundedQueueV5(2);
+//        final BoundedQueue queue = new BoundedQueueV5(2);
 
+//        final BoundedQueue queue = new BoundedQueueV6_1(2);
+//        final BoundedQueue queue = new BoundedQueueV6_2(2);
+        final BoundedQueue queue = new BoundedQueueV6_3(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
         producerFirst(queue); // 생산자 먼저 실행
