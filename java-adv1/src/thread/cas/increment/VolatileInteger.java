dd67bc098e3c1bcb0a4d626106264e1e596ecd9a
saechimdaeki/junseong <anima94@kakao.com>
2024-09-28 21:11:44 +0900

    study: 원자적 연산 - 성능 테스트

--- java-adv1/src/thread/cas/increment/VolatileInteger.java
+++ java-adv1/src/thread/cas/increment/VolatileInteger.java
@@ -1,5 +1,9 @@
 package thread.cas.increment;
 
+/**
+ * 안전한 임계 영역이 없기에 멀티 스레드 상황에서 사용할 수 없음.
+ * 단일 스레드가 사용하기에는 BingInteger보다 느리다. 멀티스레드 상황에도 안전하지 않다
+ */
 public class VolatileInteger implements IncrementInteger{
 
     private volatile int value;
