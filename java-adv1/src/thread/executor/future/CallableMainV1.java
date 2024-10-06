2512aa6da1e381f9a7533446f0979e57d6535a88
saechimdaeki/junseong <anima94@kakao.com>
2024-10-06 09:30:21 +0900

    study: Future 소개

---
+++ java-adv1/src/thread/executor/future/CallableMainV1.java
@@ -0,0 +1,34 @@
+package thread.executor.future;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableMainV1 {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(1);
+        Future<Integer> future = es.submit(new MyCallable());
+        Integer result = future.get();
+        log("result value = " + result);
+        es.close();
+    }
+
+    static class MyCallable implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            log("Callable 시작");
+            sleep(2000);
+            int value = new Random().nextInt(10);
+            log("Callable 완료");
+            return value;
+        }
+    }
+}
